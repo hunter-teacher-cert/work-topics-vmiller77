a14c2176a2c59b9f700f0e163d8b575d88eef51f
Victoria (Miller) Berkowitz <vmiller77@users.noreply.github.com>
2022-04-11 18:22:44 -0400

    Create bellpepper.py

---
+++ cipher/bellpepper/bellpepper.py
@@ -0,0 +1,60 @@
+#In Class Demo
+#encrypt letters such that capital Z shifted 1 is capital A and lowercase z shifted 1 is lowercase a
+def encrypt(text,s):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        # traverse the plain text
+        if(char.isupper()):
+        # Encrypt uppercase characters in plain text
+        # convert letter into unicode
+        # make A unicode 0 by subtracting 65
+        # add shift
+        # allow wraparound by mod 26
+        # make A's unicode 65 again by adding 65
+        # convert unicode to letter
+            result += chr((ord(char) - 65 + s) % 26 + 65)
+        else:
+            # Async Work: Encrypt lowercase characters in plain text
+            #Make a unicode 0 by subtracting 97
+            # add shift
+            #allow wraparound by mod 26
+            #make a's unicode 97 again by adding 97
+            #convert unicode to letter
+            result += chr((ord(char) - 97 + s) % 26 + 97)
+    return result
+
+  #enter alphabetic text without spaces and a desired shift
+text = "AZaz"
+s = 27
+
+print("Plain Text : " + text)
+print("Shift pattern : " + str(s))
+print("Encrypted Text: " + encrypt(text,s))
+
+#Homework Bell Pepper Version: Write a function that decrypts a symmetric cipher with a given shift
+text = "BAba"
+s = 27
+
+def decrypt(text,s):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        #uppercase
+        if(char.isupper()):
+            if(ord(char)-(s%26)==64):
+                result += 'Z'
+            else:
+                result += chr(ord(char)-(s%26))
+        #lowercase
+        else:
+            if(ord(char)-(s%26)==96):
+                result += 'Z'
+            else:
+                result += chr(ord(char)-(s%26))
+            
+    return result
+
+print("Encrypted Text : " + text)
+print("Shift pattern : " + str(s))
+print("Decrypted Text: " + decrypt(text,s))
